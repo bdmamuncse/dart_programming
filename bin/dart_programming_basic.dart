3ec286a6286366f375161c8e9942b67e97661e36
sazzad <bdmamuncse@gmail.com>
2021-11-27 22:07:00 +0600

    this is second commit

--- bin/dart_programming_basic.dart
+++ bin/dart_programming_basic.dart
@@ -1,5 +1,58 @@
 void main() {
-  var name='sazzad';
+  // var name='sazzad';
+  //
+  // print('Name: $name');
 
-  print('Name: $name');
+  // myMethodName();
+  // print(myId());
+  // mylist();
+
+  // var myIdResult=3.2;
+  //var myIdResult=myId();
+  //int myIdResult=myId();
+  // print(myIdResult);
+  // print(mylist());
+  // print(myMapMethod());
+
+  //argument/parameter-> two types
+  //1)normal-argument 2)positional argument
+  // displayWithNormalArgument('asdfadsfafas');
+  displayWithoutNormalArgument(yourId:'110', yourName: 'Sazzad');
+
 }
+
+void displayWithNormalArgument(String msg){
+  print('Your message is: $msg');
+}
+
+void displayWithoutNormalArgument({String? yourId, String? yourName}){
+  print('Your_id:$yourId Your_Name:$yourName');
+}
+
+void myMethodName(){
+print('My MethodName is called');
+}
+
+int myId(){
+  print('MyID called');
+  return 14;
+}
+
+String myName(){
+  return 'My Name is Sazzad';
+}
+
+double myCGPA(){
+  return 1.5;
+}
+
+List<String> mylist(){
+  print('Mylist called');
+  return ['name-1', 'name-2'];
+  // print('Mylist called');
+}
+
+Map<String, String> myMapMethod(){
+  return {'key':'value'};
+}
+
